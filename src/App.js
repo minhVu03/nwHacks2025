7ba22d63276d6fc9a4c1a39e98fe7d1fa34556a5
minhVu03 <minhvuams@gmail.com>
2025-01-19 00:50:46 -0800

    fixed merge conflicts components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import foodBankImage from'./images/food-bank.png'
-import megaphoneImage from'./images/megaphone.png'
+import foodBankImage from './images/food-bank.png';
+import megaphoneImage from './images/megaphone.png';
 
 function App() {
   const [showChatbox, setShowChatbox] = useState(false);
@@ -45,158 +45,91 @@ function App() {
 
   return (
     <div className="App">
-      {/* Existing components */}
+      {/* Landing Section */}
       <div className="landing">
-        <div className="landing-left-box"> 
+        <div className="landing-left-box">
           <h2>Fire Warrior</h2>
-          <span>All in 1 website to help you get ready for wildfires & evacuation</span>
+          <span>
+            All in 1 website to help you get ready for wildfires & evacuation
+          </span>
         </div>
         <div>
           <div>MAP</div>
           <button>Search Wildfires In The Area</button>
         </div>
       </div>
-      <div className="body">
-        <div className="row row-one">
-          <div className="big-card style-border">
-            <p className="box-title">Air Quality</p>
-          </div>
-          <div className="big-card style-border">
-            <p className="box-title">News & Media</p>
-          </div>
-        </div>
-        <div className="row row-two style-border">
-          <div className="big-card style-border">
-            <span className="box-title">Food Banks</span>
-            <div className="small-card style-border">
-              <span>title</span>
-              <span>Address</span>
-              <span>Phone</span>
-              <span>Hour</span>
-            </div>
-            <div className="small-card style-border">
-              <span>title</span>
-              <span>Address</span>
-              <span>Phone</span>
-              <span>Hour</span>
 
-            <div className="big-card air style-border">
-              <span className="box-title">Air Quality</span>
-              <div className="score-box">
-                  <span>43</span>
-                  <span>Good</span>
-                  <div>
-                    <p>Health Advisory</p>
-                    <span>Air quality is satisfactory. Outdoor activities are safe.</span>
-                  </div>
-              </div>
-            </div>
-            <div className="big-card news style-border">
-              <span className="box-title">News & Media</span>
-              <div className="news-card-wrapper">
-                <div className="news-card style-border">
-                  <img src={megaphoneImage}></img>
-                  <div className="news-info ">
-                    <span>title</span>
-                    <span>desc</span>
-                    <span>date</span>
-                  </div>
-                </div>
-                <div className="news-card style-border">
-                  <img src={megaphoneImage}></img>
-                  <div className="news-info">
-                    <span>title</span>
-                    <span>desc</span>
-                    <span>date</span>
-                  </div>
-                </div>
-                <div className="news-card style-border">
-                  <img src={megaphoneImage}></img>
-                  <div className="news-info">
-                    <span>title</span>
-                    <span>desc</span>
-                    <span>date</span>
-                  </div>
-                </div>
-                <div className="news-card style-border">
-                  <img src={megaphoneImage}></img>
-                  <div className="news-info">
-                    <span>title</span>
-                    <span>desc</span>
-                    <span>date</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-        </div>
+      {/* Body Section */}
+      <div className="body">
         <div className="row row-two">
-            <div className="big-card food-bank style-border">
-              <span className="box-title">Food Banks</span>
-              <div className="small-card-wrapper">
-                <div className="small-card style-border">
-                  <span>Kamloops Food Bank</span>
-                  <img src={foodBankImage}></img>
-                  <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
-                  <span>Phone: +1 250-376-2252</span>
-                  <span>Hours: Mon-Fri: 
-                  8:00 AM - 2:00 PM</span>
-                </div>
-                <div className="small-card style-border">
-                  <span>Kamloops Food Bank</span>
-                  <img src={foodBankImage}></img>
-                  <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
-                  <span>Phone: +1 250-376-2252</span>
-                  <span>Hours: Mon-Fri: 
-                  8:00 AM - 2:00 PM</span>
-                </div>
-              
+          <div className="big-card air style-border">
+            <span className="box-title">Air Quality</span>
+            <div className="score-box">
+              <span>43</span>
+              <span>Good</span>
+              <div>
+                <p>Health Advisory</p>
+                <span>
+                  Air quality is satisfactory. Outdoor activities are safe.
+                </span>
               </div>
             </div>
+          </div>
+          <div className="big-card news style-border">
+            <span className="box-title">News & Media</span>
+            <div className="news-card-wrapper">
+              {[...Array(4)].map((_, index) => (
+                <div key={index} className="news-card style-border">
+                  <img src={megaphoneImage} alt="News" />
+                  <div className="news-info">
+                    <span>title</span>
+                    <span>desc</span>
+                    <span>date</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
+
+        <div className="row row-two">
+          <div className="big-card food-bank style-border">
+            <span className="box-title">Food Banks</span>
+            <div className="small-card-wrapper">
+              {[...Array(2)].map((_, index) => (
+                <div key={index} className="small-card style-border">
+                  <span>Kamloops Food Bank</span>
+                  <img src={foodBankImage} alt="Food Bank" />
+                  <span>
+                    Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada
+                  </span>
+                  <span>Phone: +1 250-376-2252</span>
+                  <span>Hours: Mon-Fri: 8:00 AM - 2:00 PM</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
         <div className="row row-three">
           <div className="big-card hospital style-border">
-              <span className="box-title">Hospitals & Shelters</span>
-              <div className="small-card-wrapper hospital">
-                <div className="small-card style-border">
-                    <span>Kamloops Food Bank</span>
-                    <img src={foodBankImage}></img>
-                    <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
-                    <span>Phone: +1 250-376-2252</span>
-                    <span>Hours: Mon-Fri: 
-                    8:00 AM - 2:00 PM</span>
+            <span className="box-title">Hospitals & Shelters</span>
+            <div className="small-card-wrapper hospital">
+              {[...Array(4)].map((_, index) => (
+                <div key={index} className="small-card style-border">
+                  <span>Kamloops Food Bank</span>
+                  <img src={foodBankImage} alt="Food Bank" />
+                  <span>
+                    Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada
+                  </span>
+                  <span>Phone: +1 250-376-2252</span>
+                  <span>Hours: Mon-Fri: 8:00 AM - 2:00 PM</span>
                 </div>
-                <div className="small-card style-border">
-                    <span>Kamloops Food Bank</span>
-                    <img src={foodBankImage}></img>
-                    <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
-                    <span>Phone: +1 250-376-2252</span>
-                    <span>Hours: Mon-Fri: 
-                    8:00 AM - 2:00 PM</span>
-                </div>
-                <div className="small-card style-border">
-                    <span>Kamloops Food Bank</span>
-                    <img src={foodBankImage}></img>
-                    <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
-                    <span>Phone: +1 250-376-2252</span>
-                    <span>Hours: Mon-Fri: 
-                    8:00 AM - 2:00 PM</span>
-                </div>
-                <div className="small-card style-border">
-                    <span>Kamloops Food Bank</span>
-                    <img src={foodBankImage}></img>
-                    <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
-                    <span>Phone: +1 250-376-2252</span>
-                    <span>Hours: Mon-Fri: 
-                    8:00 AM - 2:00 PM</span>
-                </div>
-              </div>
-
+              ))}
             </div>
           </div>
-          <div className="big-card style-border">
-            <span className="box-title">Hospitals & Shelters</span>
-          </div>
         </div>
+
         <div className="row row-four style-border">
           <div>
             <span>checklist</span>
@@ -204,7 +137,7 @@ function App() {
         </div>
       </div>
 
-      {/* Chatbot icon */}
+      {/* Chatbot Icon */}
       <div className="chat-icon" onClick={toggleChatbox}>
         💬
       </div>
