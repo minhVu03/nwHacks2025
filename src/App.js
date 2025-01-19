a8a30df4f6a4e4f54649ea61d09059dcefe8f096
Hannah Li <hli149@students.cs.ubc.ca>
2025-01-19 00:26:06 -0800

    styled the cards & boxes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import foodBankImage from'./images/food-bank.png'
+import megaphoneImage from'./images/megaphone.png'
 
 function App() {
   return (
@@ -16,34 +18,119 @@ function App() {
       </div>
       <div className="body">
         <div className="row row-one">
-            <div className="big-card style-border">
-              <p className="box-title">Air Quality</p>
+            <div className="big-card air style-border">
+              <span className="box-title">Air Quality</span>
+              <div className="score-box">
+                  <span>43</span>
+                  <span>Good</span>
+                  <div>
+                    <p>Health Advisory</p>
+                    <span>Air quality is satisfactory. Outdoor activities are safe.</span>
+                  </div>
+              </div>
             </div>
-            <div className="big-card style-border">
-              <p className="box-title">News & Media</p>
+            <div className="big-card news style-border">
+              <span className="box-title">News & Media</span>
+              <div className="news-card-wrapper">
+                <div className="news-card style-border">
+                  <img src={megaphoneImage}></img>
+                  <div className="news-info ">
+                    <span>title</span>
+                    <span>desc</span>
+                    <span>date</span>
+                  </div>
+                </div>
+                <div className="news-card style-border">
+                  <img src={megaphoneImage}></img>
+                  <div className="news-info">
+                    <span>title</span>
+                    <span>desc</span>
+                    <span>date</span>
+                  </div>
+                </div>
+                <div className="news-card style-border">
+                  <img src={megaphoneImage}></img>
+                  <div className="news-info">
+                    <span>title</span>
+                    <span>desc</span>
+                    <span>date</span>
+                  </div>
+                </div>
+                <div className="news-card style-border">
+                  <img src={megaphoneImage}></img>
+                  <div className="news-info">
+                    <span>title</span>
+                    <span>desc</span>
+                    <span>date</span>
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
-        <div className="row row-two style-border">
-            <div className="big-card style-border">
+        <div className="row row-two">
+            <div className="big-card food-bank style-border">
               <span className="box-title">Food Banks</span>
-              <div className="small-card style-border">
-                <span>title</span>
-                <span>Address</span>
-                <span>Phone</span>
-                <span>Hour</span>
+              <div className="small-card-wrapper">
+                <div className="small-card style-border">
+                  <span>Kamloops Food Bank</span>
+                  <img src={foodBankImage}></img>
+                  <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
+                  <span>Phone: +1 250-376-2252</span>
+                  <span>Hours: Mon-Fri: 
+                  8:00 AM - 2:00 PM</span>
+                </div>
+                <div className="small-card style-border">
+                  <span>Kamloops Food Bank</span>
+                  <img src={foodBankImage}></img>
+                  <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
+                  <span>Phone: +1 250-376-2252</span>
+                  <span>Hours: Mon-Fri: 
+                  8:00 AM - 2:00 PM</span>
+                </div>
+              
               </div>
-              <div className="small-card style-border">
-                <span>title</span>
-                <span>Address</span>
-                <span>Phone</span>
-                <span>Hour</span>
-              </div>
-            </div>
-            <div className="big-card style-border">
-              <span className="box-title">Hospitals & Shelters</span>
             </div>
         </div>
-        <div className="row row-three style-border">
+        <div className="row row-three">
+          <div className="big-card hospital style-border">
+              <span className="box-title">Hospitals & Shelters</span>
+              <div className="small-card-wrapper hospital">
+                <div className="small-card style-border">
+                    <span>Kamloops Food Bank</span>
+                    <img src={foodBankImage}></img>
+                    <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
+                    <span>Phone: +1 250-376-2252</span>
+                    <span>Hours: Mon-Fri: 
+                    8:00 AM - 2:00 PM</span>
+                </div>
+                <div className="small-card style-border">
+                    <span>Kamloops Food Bank</span>
+                    <img src={foodBankImage}></img>
+                    <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
+                    <span>Phone: +1 250-376-2252</span>
+                    <span>Hours: Mon-Fri: 
+                    8:00 AM - 2:00 PM</span>
+                </div>
+                <div className="small-card style-border">
+                    <span>Kamloops Food Bank</span>
+                    <img src={foodBankImage}></img>
+                    <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
+                    <span>Phone: +1 250-376-2252</span>
+                    <span>Hours: Mon-Fri: 
+                    8:00 AM - 2:00 PM</span>
+                </div>
+                <div className="small-card style-border">
+                    <span>Kamloops Food Bank</span>
+                    <img src={foodBankImage}></img>
+                    <span>Address: 171 Wilson St, Kamloops, BC V2B 2M4, Canada</span>
+                    <span>Phone: +1 250-376-2252</span>
+                    <span>Hours: Mon-Fri: 
+                    8:00 AM - 2:00 PM</span>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div className="row row-four style-border">
           <div>
             <span>checklist</span>
           </div>
